b4be85f26674f2b7b49e5fda67c14f27ef954506
brunoamaia <brunoamaia94@gmail.com>
2020-08-03 16:19:19 -0300

    Criar Landing page e estilo mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import './assets/styles/global.css';
 
+import Landing from './pages/landing';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    <Landing />
+  )
 }
 
 export default App;
