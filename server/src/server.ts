a72051bf6ac8d71ece9edd0cbcc741200b247476
brunoamaia <brunoamaia94@gmail.com>
2020-08-04 12:02:50 -0300

    limpando o arquivo

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,13 +16,8 @@ Route Params: identificar quala recurso eu quero atualizar ou deletar
 Query Params: Parametros passados com a rota (paginação. filtros, ordenação)
 */
 
-app.get('/users', (request, response) => {
-    const users = [
-        {name: 'Fulano', age: 25},
-        {name: 'Ciclano', age: 22},
-    ];
-    
-    return response.json(users)
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World'})
 })
 
 app.listen(3333);   // "Escuta" as requisições Http, na porta 3333
