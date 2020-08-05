83a49c68e137de3d332694d090c49277c436fb57
brunoamaia <brunoamaia94@gmail.com>
2020-08-05 14:23:02 -0300

    Pegar a quatida de  conexões

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landing from '../../assets/images/landing.svg';
@@ -12,6 +14,16 @@ import './styles.css';
 
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total)
+        })
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -35,7 +47,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas
+                    Total de {totalConnections} conexões já realizadas
                     <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span> 
             </div>
