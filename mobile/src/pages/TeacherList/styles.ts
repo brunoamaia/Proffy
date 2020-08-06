90af0daa47d1965ee83505d47350dad448c127b6
brunoamaia <brunoamaia94@gmail.com>
2020-08-06 16:01:33 -0300

    Navegação em abas

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -4,9 +4,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8257E5',
-    justifyContent: 'center',
-    padding: 40
+    backgroundColor: '#F0F0F7',
   },
 
 })
